1b0813bd2e1acdf1edb3bd3a08bb51235ed28447
Jan-Philipp Rathje <Jan-Philipp.Rathje@rewe-group.com>
2020-03-28 22:04:33 +0100

    Initial commit

---
+++ src/main/java/de/fhkiel/advancedjava/PublicTransportationPlatformApplication.java
@@ -0,0 +1,13 @@
+package de.fhkiel.advancedjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicTransportationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicTransportationPlatformApplication.class, args);
+	}
+
+}
