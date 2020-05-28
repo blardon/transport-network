8abfc0c3b0aebf2028896578b1d35a4043235c36
blardon <bennetvlardon@gmail.com>
2020-05-28 15:06:04 +0200

    added Relationship models

--- src/main/java/de/fhkiel/advancedjava/model/relationship/ConnectingTo.java
+++ src/main/java/de/fhkiel/advancedjava/model/relationship/ConnectingTo.java
@@ -2,13 +2,16 @@ package de.fhkiel.advancedjava.model.relationship;
 
 import de.fhkiel.advancedjava.model.node.Leg;
 import de.fhkiel.advancedjava.model.node.Stop;
+import lombok.*;
 import org.neo4j.ogm.annotation.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @RelationshipEntity(type = "CONNECTING_TO")
 public class ConnectingTo {
 
     @Id @GeneratedValue
-    private Long connectingToId;
+    private Long id;
 
     @StartNode
     private Leg connectingWithLeg;
@@ -16,6 +19,33 @@ public class ConnectingTo {
     @EndNode
     private Stop connectingToStop;
 
-    private Long time;
+    private Long time = 0L;
 
+    public Long getConnectingToId() {
+        return id;
+    }
+
+    public Leg getConnectingWithLeg() {
+        return connectingWithLeg;
+    }
+
+    public void setConnectingWithLeg(Leg connectingWithLeg) {
+        this.connectingWithLeg = connectingWithLeg;
+    }
+
+    public Stop getConnectingToStop() {
+        return connectingToStop;
+    }
+
+    public void setConnectingToStop(Stop connectingToStop) {
+        this.connectingToStop = connectingToStop;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
 }
--- src/main/java/de/fhkiel/advancedjava/model/relationship/TransferTo.java
+++ src/main/java/de/fhkiel/advancedjava/model/relationship/TransferTo.java
@@ -2,13 +2,16 @@ package de.fhkiel.advancedjava.model.relationship;
 
 import de.fhkiel.advancedjava.model.node.Station;
 import de.fhkiel.advancedjava.model.node.Stop;
+import lombok.*;
 import org.neo4j.ogm.annotation.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @RelationshipEntity(type = "TRANSFER_TO")
 public class TransferTo {
 
     @Id @GeneratedValue
-    private Long transferToId;
+    private Long id;
 
     @StartNode
     private Stop fromStop;
@@ -16,6 +19,33 @@ public class TransferTo {
     @EndNode
     private Station toStation;
 
-    private Long time;
+    private Long time = 0L;
 
+    public Long getTransferToId() {
+        return id;
+    }
+
+    public Stop getFromStop() {
+        return fromStop;
+    }
+
+    public void setFromStop(Stop fromStop) {
+        this.fromStop = fromStop;
+    }
+
+    public Station getToStation() {
+        return toStation;
+    }
+
+    public void setToStation(Station toStation) {
+        this.toStation = toStation;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
 }
