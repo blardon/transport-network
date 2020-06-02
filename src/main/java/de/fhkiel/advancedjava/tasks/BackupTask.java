57f3963e6c50ab6f50906852fc4430dcaf5918e7
blardon <bennetvlardon@gmail.com>
2020-06-02 14:18:42 +0200

    Added backup task

---
+++ src/main/java/de/fhkiel/advancedjava/tasks/BackupTask.java
@@ -0,0 +1,79 @@
+package de.fhkiel.advancedjava.tasks;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.fhkiel.advancedjava.model.node.dto.LineDto;
+import de.fhkiel.advancedjava.model.node.dto.ScheduleDto;
+import de.fhkiel.advancedjava.model.node.dto.StationDto;
+import de.fhkiel.advancedjava.service.DtoConversionService;
+import de.fhkiel.advancedjava.service.LineService;
+import de.fhkiel.advancedjava.service.StationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+@Service
+public class BackupTask {
+
+    @Value("${backup.path}")
+    private String EXPORT_FILE_PATH;
+
+    private static final Logger log = LoggerFactory.getLogger(BackupTask.class);
+    private ObjectMapper objectMapper;
+
+    private StationService stationService;
+    private LineService lineService;
+    private DtoConversionService conversionService;
+
+    @Autowired
+    public BackupTask(ObjectMapper objectMapper, StationService stationService, LineService lineService, DtoConversionService conversionService){
+        this.objectMapper = objectMapper;
+        this.stationService = stationService;
+        this.lineService = lineService;
+        this.conversionService = conversionService;
+    }
+
+    @Scheduled(cron = "${backup.cron}")
+    private void doBackup(){
+        log.info("DOING BACKUP...");
+
+        ScheduleDto scheduleDto = new ScheduleDto();
+
+        // Convert all Stations to StationDTOs and collect them
+        ArrayList<StationDto> stationDtos = this.stationService.findAllStationsWithStops()
+                .stream()
+                .map(station -> this.conversionService.convert(station))
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        // Convert all Lines to LineDTOs and collect them
+        ArrayList<LineDto> lineDtos = this.lineService.findAllLinesWithLegs().stream()
+                .map(line -> this.conversionService.convert(line))
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        scheduleDto.setStations(stationDtos);
+        scheduleDto.setLines(lineDtos);
+
+        try {
+            File target = new File(EXPORT_FILE_PATH);
+            if (target.exists())
+                target.delete();
+            if (target.createNewFile()){
+                this.objectMapper.writeValue(target, scheduleDto);
+                log.info("Saved backup to " + target.getAbsolutePath());
+            }else{
+                log.warn("Could not save backup to " + target.getAbsolutePath());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
